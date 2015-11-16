a55552a47966581ec1a51d44524467765eea258b
Mario FLores <pokazzam@gmail.com>
2015-11-15 19:45:57 -0600

    new menu added, with functions (included to map) and styles updated

--- client/components/main/menu.directive.js
+++ client/components/main/menu.directive.js
@@ -2,8 +2,34 @@
 	'use strict';
 
 	angular.module('menu.directive', [])
-	.directive('menuNavigation', ['$timeout',function ($timeout) {
-		var dotMenu = angular.element(document.getElementById('dotmenu'));
+	.directive('menuNavigation', ['$timeout', '$document','$state',function ($timeout, $document, $state) {
+		return{
+			restrict: 'A',
+			controller: function(){
+				var body = angular.element(document.getElementsByTagName('body'));
+				var menu = angular.element(document.getElementsByClassName('navbar-right'));
+				var clickItem = menu.children().children();
+				$document.on('scroll', function() {
+					var topHeader = angular.element(document.getElementsByClassName('navbar-default'));
+					if($document.scrollTop() >= 65){
+						topHeader.addClass('sticky');
+					}
+					else if($state.current.name === 'mapa'){
+						topHeader.addClass('sticky');
+					}
+					else{
+						topHeader.removeClass('sticky');
+					}
+				});
+				
+				if (body[0].clientWidth <= 768) {
+					clickItem.bind('click', function(){
+						menu.parent().removeClass('in');
+					})
+				};
+			}
+		}
+		/*var dotMenu = angular.element(document.getElementById('dotmenu'));
 		var item = dotMenu.children().children('.clickItem');
 		var body = angular.element(document.getElementsByTagName('body'));
 		var bodyClass = angular.element(document.getElementsByClassName('st-pusher'));
@@ -26,6 +52,7 @@
 				});
 			}
 		};
+		*/
 	}]);
 
 })();
