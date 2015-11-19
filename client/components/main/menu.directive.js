944157474772a3ec108eecdcf395b2fd4e69d5fb
Mario FLores <pokazzam@gmail.com>
2015-11-19 13:09:32 -0600

    removed commented block

--- client/components/main/menu.directive.js
+++ client/components/main/menu.directive.js
@@ -29,30 +29,6 @@
 				};
 			}
 		}
-		/*var dotMenu = angular.element(document.getElementById('dotmenu'));
-		var item = dotMenu.children().children('.clickItem');
-		var body = angular.element(document.getElementsByTagName('body'));
-		var bodyClass = angular.element(document.getElementsByClassName('st-pusher'));
-		return {
-			restrict: 'E',
-			template: '<a class="open-menu"><i class="fa fa-reorder"></i></a>',
-			link: function (scope,element, attrs) {
-				element.bind('click', function(e){
-					e.preventDefault();
-					dotMenu.addClass('show');
-
-					item.bind('click', function(){
-						dotMenu.removeClass('show');
-					});
-					
-					bodyClass.bind('click', function(){
-							dotMenu.removeClass('show');
-					});
-
-				});
-			}
-		};
-		*/
 	}]);
 
 })();
