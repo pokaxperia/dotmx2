bf6eb02b83ce584b65d8563c85803ed9832a8955
Mario FLores <pokazzam@gmail.com>
2015-12-15 15:38:38 -0600

    removed html5mode

--- client/components/routes.js
+++ client/components/routes.js
@@ -2,42 +2,42 @@
 	'use strict';
 	
 	angular.module('routes', ['ui.router'])
-	.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/');
 		$stateProvider
-			.state('/', {
+			.state('main', {
 				url: '/',
 				controller: 'MainController',
-				templateUrl: 'components/main/main.html'
+				templateUrl: './components/main/main.html'
 			})
 			.state('instrumentos', {
 				url: '/instrumentos',
 				controller: 'InstrumentosController',
-				templateUrl: 'components/instrumentos/instrumentos.html'
+				templateUrl: './components/instrumentos/instrumentos.html'
 			})
 			.state('guia', {
 				url: '/guia',
 				controller: 'GuiaController',
-				templateUrl: 'components/guia/guia.html'
+				templateUrl: './components/guia/guia.html'
 			})
 			.state('mapa', {
 				url: '/mapa',
 				controller: 'MapaController',
-				templateUrl: 'components/mapa/mapa.html'
+				templateUrl: './components/mapa/mapa.html'
 			})
 			.state('estrategias', {
 				url: '/estrategias',
 				controller: 'EstrategiasController',
-				templateUrl: 'components/estrategias/estrategias.html'
+				templateUrl: './components/estrategias/estrategias.html'
 			})
 			.state('modelo', {
 				url: '/modelo',
 				controller: 'ModeloController',
-				templateUrl: 'components/modelo/modelo.html'
+				templateUrl: './components/modelo/modelo.html'
 			})
 			.state('descargas', {
 				url: '/descargas',
-				templateUrl: 'components/descargas/descargas.html'
+				templateUrl: './components/descargas/descargas.html'
 			});
 	}]);
 
